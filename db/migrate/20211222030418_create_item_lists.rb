b8f047286f7a59a6e5be091f28002213be4d737c
yunkmr <yuuun.k16@gmail.com>
2021-12-25 02:32:59 +0000

    Initialize Commit

---
+++ db/migrate/20211222030418_create_item_lists.rb
@@ -0,0 +1,11 @@
+class CreateItemLists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :item_lists do |t|
+      t.user_id :integer
+      t.event_id :integer
+
+
+      t.timestamps
+    end
+  end
+end
