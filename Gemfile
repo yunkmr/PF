2613c168139823cb6d098a73692b5cf48eb61bcf
yunkmr <yuuun.k16@gmail.com>
2021-12-29 02:51:08 +0000

    [Add]devise追加

--- Gemfile
+++ Gemfile
@@ -62,3 +62,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'simple_calendar', '~> 2.0'
+
+gem 'devise'
--- db/migrate/20211222030418_create_item_lists.rb
+++ db/migrate/20211222030418_create_item_lists.rb
@@ -1,8 +1,8 @@
 class CreateItemLists < ActiveRecord::Migration[5.2]
   def change
     create_table :item_lists do |t|
-      t.user_id :integer
-      t.event_id :integer
+      # t.user_id :integer
+      # t.event_id :integer
 
 
       t.timestamps
